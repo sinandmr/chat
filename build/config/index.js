1681ebdba23702fee420d4d377a4cdc47ce015c6
Sinan Demir <sinanndmrr@gmail.com>
2023-06-18 18:05:44 +0300

    fix

--- build/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => ({
-    PORT: parseInt(process.env.PORT, 10) || 3028,
-    NODE_ENV: process.env.NODE_ENV,
-    DATABASE_URI: process.env.DATABASE_URI,
-});
--- src/config/index.ts
+++
@@ -1,5 +0,0 @@
-export default () => ({
-  PORT: parseInt(process.env.PORT, 10) || 3028,
-  NODE_ENV: process.env.NODE_ENV,
-  DATABASE_URI: process.env.DATABASE_URI,
-});
